eeacfffb68768745bc77d0bafedbd4617589b507
delena <delenamaneki@gmail.com>
2021-09-25 00:09:30 +0700

    Basic flow

--- programs/staking/src/lib.rs
+++ programs/staking/src/lib.rs
@@ -1,15 +1,65 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{
+    log::{sol_log_compute_units, sol_log_params, sol_log_slice},
+    pubkey::Pubkey,
+};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod staking {
+
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        msg!("HIIIIIIIIIIIIIIII");
+    pub fn init(ctx: Context<InitMessage>, authority: Pubkey, amount: u64) -> ProgramResult {
+        let ref mut data = ctx.accounts.token;
+        data.amount = amount;
+        data.authority = authority;
+
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<DepositMessage>, amount: u64) -> ProgramResult {
+        let ref mut data = ctx.accounts.token;
+        data.amount += amount;
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<WithDraw>, amount: u64) -> ProgramResult {
+        let ref mut data = ctx.accounts.token;
+        data.amount -= amount;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitMessage<'info> {
+    #[account(init, payer = user, space = 8 + 40)]
+    pub token: Account<'info, Token>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct DepositMessage<'info> {
+    #[account(mut, has_one = authority)]
+    pub token: Account<'info, Token>,
+
+    pub authority: Signer<'info>,
+}
+#[derive(Accounts)]
+pub struct WithDraw<'info> {
+    #[account(mut, has_one = authority)]
+    pub token: Account<'info, Token>,
+
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct Token {
+    pub authority: Pubkey,
+    pub amount: u64,
+}
--- tests/staking.js
+++ tests/staking.js
@@ -1,13 +1,43 @@
 const anchor = require("@project-serum/anchor");
+const assert = require("assert");
+const { BN } = require("bn.js");
 
-describe("staking", () => {
+const { SystemProgram } = anchor.web3;
+
+describe("Test staking", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.local();
 
-  it("Is initialized!", async () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(provider);
+
+  const alice = anchor.web3.Keypair.generate();
+  const bob = anchor.web3.Keypair.generate();
+
+  const program = anchor.workspace.Staking;
+
+  it("Init", async () => {
     // Add your test here.
-    const program = anchor.workspace.Staking;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+    await program.rpc.init(alice.publicKey, new BN(200), {
+      accounts: {
+        token: alice.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [alice],
+    });
+
+    console.log(program.account.token);
+    let aliceData = await program.account.token.fetch(alice.publicKey);
+    assert.ok(aliceData.amount.toNumber() === 200);
   });
+
+  //   it("With draw", async () => {
+  //     await program.rpc.withdraw(new BN(100), {
+  //       accounts: {
+  //         tokens: alice.publicKey,
+  //         authority: provider.wallet.publicKey,
+  //       },
+  //     });
+  //   });
 });
