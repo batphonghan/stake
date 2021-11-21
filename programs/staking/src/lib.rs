148171e6e763c68f5a43dd0dfc3c19510294ec2f
batphonghan <vanhungdeveloper@gmail.com>
2021-11-21 11:32:26 +0700

    Init

--- programs/staking/src/lib.rs
+++ programs/staking/src/lib.rs
@@ -12,6 +12,12 @@ pub mod staking {
 
     use super::*;
 
+    pub fn init(ctx: Context<Init>, amount: u64) -> ProgramResult {
+        let ref mut counter = ctx.accounts.counter;
+        counter.amount = amount;
+        Ok(())
+    }
+
     pub fn deposit(ctx: Context<Deposit>, amount: u64) -> ProgramResult {
         let cpi_accounts = Transfer {
             from: ctx.accounts.depositor.to_account_info().clone(),
@@ -24,6 +30,17 @@ pub mod staking {
 
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct Init<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub amount: u64,
 }
 
 #[derive(Accounts)]
